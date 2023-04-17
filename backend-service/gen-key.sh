6db9f37d53835040baede146e63cb9c793f82447
Phuong <pmphan00@gmail.com>
2023-04-17 17:58:48 -0400

    [Backend] Add a flag file and a gen-key file.

---
+++ backend-service/gen-key.sh
@@ -0,0 +1,5 @@
+mkdir -p secret
+openssl ecparam -genkey -name prime256v1 -noout -out secret/jwt.key
+openssl ec -in secret/jwt.key -pubout > secret/jwt-pub.pub
+ssh-keygen -y -f secret/jwt.key > secret/jwt.pub
+echo "CTF_SDaT{N91nx_41145}" >> secret/jwt.pub

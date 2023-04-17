a13aa905ed4c603d474f82e41b41f58126947bed
Phuong <pmphan00@gmail.com>
2023-04-17 19:31:55 -0400

    [Chore] Change secret locations.

--- gen-key.sh
+++ gen-key.sh
@@ -2,4 +2,4 @@ mkdir -p secret
 openssl ecparam -genkey -name prime256v1 -noout -out secret/jwt.key
 openssl ec -in secret/jwt.key -pubout > secret/jwt-pub.pub
 ssh-keygen -y -f secret/jwt.key > secret/jwt.pub
-echo "CTF_SDaT{N91nx_41145}" >> secret/jwt.pub
+echo "CTF_SDaT{N91nx_41145}" > secret/flag
